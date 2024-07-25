82f9caeb2b19546fff90abadb810a2133071b0c6
Justine RAGUES <justineragues@gmail.com>
2024-07-25 12:12:19 +0200

    TP2

--- src/test/java/java17/ex02/Lambda_02_Test.java
+++ src/test/java/java17/ex02/Lambda_02_Test.java
@@ -13,16 +13,18 @@ import java.util.List;
 /**
  * Exercice 02 - Map
  */
-public class Lambda_02_Test {
-
+public class Lambda_02_Test
+{
     // tag::PersonToAccountMapper[]
-    interface PersonToAccountMapper {
+    interface PersonToAccountMapper
+    {
         Account map(Person p);
     }
     // end::PersonToAccountMapper[]
 
     // tag::map[]
-    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
+    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper)
+    {
         List<Account> accounts = new ArrayList<>();
         personList.forEach(p -> accounts.add(mapper.map(p)));
         return accounts;
@@ -32,16 +34,15 @@ public class Lambda_02_Test {
 
     // tag::test_map_person_to_account[]
     @Test
-    public void test_map_person_to_account() throws Exception {
-
+    public void test_map_person_to_account() throws Exception
+    {
         List<Person> personList = Data.buildPersonList(100);
 
-        // TODO transformer la liste de personnes en liste de comptes
-        // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        List<Account> result = map(personList, p -> new Account(p, 100));
 
         assert result.size() == personList.size();
-        for (Account account : result) {
+        for (Account account : result)
+        {
             assert account.getBalance().equals(100);
             assert account.getOwner() != null;
         }
