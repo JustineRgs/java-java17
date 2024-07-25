b107d668eca47e2d0af198ed5de6480e1dc602c5
Justine RAGUES <justineragues@gmail.com>
2024-07-25 14:21:54 +0200

    TP4

--- src/test/java/java17/ex01/Optional_01_Test.java
+++ src/test/java/java17/ex01/Optional_01_Test.java
@@ -16,35 +16,47 @@ import java17.data.Person;
  * Exercice 02 - Filter, Map
  */
 public class Optional_01_Test {
-	
-	class NotPresentException extends RuntimeException {
-		
-	}
+
+    class NotPresentException extends RuntimeException {
+
+    }
 
     @Test
     public void test_optional_ifPresent() throws Exception {
-    	
-    	List<Person> persons = Data.buildPersonList(100);
+
+        List<Person> persons = Data.buildPersonList(100);
 
         // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 18 ans
         // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
+        Optional<Person> optPerson = persons.stream()
+                .filter(p -> p.getAge() == 18)
+                .findFirst();
+
         assertThat(optPerson.isPresent(), is(true));
-        
+
         // TODO afficher la personne en question si l'optional contient une personne
+        optPerson.ifPresent(person -> {
+            System.out.println("Personne trouvée : " + person);
+        });
     }
 
     @Test(expected=NotPresentException.class)
     public void test_optional_notPresent() throws Exception {
-    	List<Person> persons = Data.buildPersonList(50);
+        List<Person> persons = Data.buildPersonList(50);
 
         // TODO rechercher dans la liste ci-dessus la 1ère personne ayant 75 ans
         // TODO utiliser la méthode "findFirst"
-        Optional<Person> optPerson = null;
-        assertThat(optPerson.isPresent(), is(false));
-        
+        Optional<Person> optPerson = persons.stream()
+                .filter(p -> p.getAge() == 75)
+                .findFirst();
+
         // TODO si la personne n'existe pas, jeter une exception NotPresentException
         // TODO utiliser la méthode "orElseThrow"
+        assertThat(optPerson.isPresent(), is(false));
 
+        optPerson.orElseThrow(() -> {
+            System.out.println("Aucune de 75 ans personne trouvée. Lancement de NotPresentException.");
+            return new NotPresentException();
+        });
     }
 }
